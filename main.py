06d388772543a8f3e79aa27f50fdd98b4f631846
sony0505 <sychen16866@gmail.com>
2024-12-05 02:11:26 +0800

    create user page with user name

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ print("Flask app created")
 # Define a function to handle requests to the root URL
 def index():
     return "<h1>Hello, World!</h1>"
+@app.route('/user/<username>')
+
+def user(username):
+    return "<h1>Hello {}</h1>".format(username)
 
 # Run the app
 if __name__ == '__main__':
